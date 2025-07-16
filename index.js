92088f7848767013ea50796e85c2e5a3db3dbe7d
Manny Hagman <jmhagman.dev@gmail.com>
2025-07-15 21:17:59 -0500

    Add get and get by id rest calls

--- index.js
+++ index.js
@@ -3,35 +3,20 @@ import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
-import mysql from 'mysql2/promise';
+import players from './routes/players.js';
 
 const app = express();
-
+const port = 3001;
 
 app.use(cors({ origin: '*' }));
-app.use(helmet)
-app.use(compression())
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
+app.use('/api/players', players);
 
-const connection = await mysql.createConnection({
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  port: process.env.SQL_PORT,
-  password: process.env.SQL_PASS,
-});
-
-try {
-  const results = await connection.query(
-    'SELECT * FROM baseball.players',
-  );
-
-  console.log(results)
-
-} catch (err) {
-  console.log(err);
-}
-
-app.listen((3001), () => {
-  console.log(`App now listening on port ${3001}`)
+app.listen(port, () => {
+  console.log(`App now listening on port ${port}`);
 });
---
+++ routes/players.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import db from '../db.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const [results] = await db.query(`SELECT * FROM baseball.players`);
+    res.status(200).send(results);
+  } catch (err) {
+    res.send(err)
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const [results] = await db.query(`SELECT * FROM baseball.players WHERE id=${req.params.id}`);
+    res.status(200).send(results[0]);
+  } catch (err) {
+    res.send(err)
+  }
+});
+
+export default router;
