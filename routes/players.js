256001380ac61c08198eb255f41a67ca70867bcc
Manny Hagman <jmhagman.dev@gmail.com>
2025-07-17 19:58:48 -0500

    Change id to player_id

--- routes/players.js
+++ routes/players.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const [results] = await db.query(`SELECT * FROM icqyziic3s8brpuj.players WHERE id=${req.params.id}`);
+    const [results] = await db.query(`SELECT * FROM icqyziic3s8brpuj.players WHERE player_id=${req.params.player_id}`);
     res.status(200).send(results[0]);
   } catch (err) {
     res.send(err)
