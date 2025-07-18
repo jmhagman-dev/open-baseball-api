4d63a9b4225d6828741b6c75bf14c4aeaefc2833
Manny Hagman <jmhagman.dev@gmail.com>
2025-07-17 19:31:38 -0500

    Fix port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import compression from 'compression';
 import players from './routes/players.js';
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*' }));
 app.use(helmet());
